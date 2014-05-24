9e1444bd5dc1ef58aa818c3320b8835df3351f9f
projektorkraft <projektorkraft@gmail.com>
2014-05-24 18:20:42 +0200

    on the way to greatness

--- Assets/Scripts/Controller/LogicController.cs
+++ Assets/Scripts/Controller/LogicController.cs
@@ -6,40 +6,87 @@ using System.Collections.Generic;
 public class LogicController : MonoBehaviour {
 
 	List<C2DCircle> forbiddenShapes;
+	List<C2DPolyArc> blackShape;
+	List<C2DPolyArc> whiteShape;
 
 	public Constants.StoneColor toPlay;
 
 	// Use this for initialization
 	void Start () {
 		forbiddenShapes = new List<C2DCircle> ();
+		blackShape = new List<C2DPolyArc>();
+		whiteShape = new List<C2DPolyArc>();
 		toPlay = Constants.StoneColor.Black;
 	}
 
 	public bool addStone(C2DPoint pos) {
 
 		Constants.StoneColor color = toPlay;
-
-		if (hasPlace (pos) && isLegal(pos, color)) {
-			makeMove (pos, color);
+		//TODO: copy here
+		List<C2DPolyArc> _blackShape = blackShape;
+		List<C2DPolyArc> _whiteShape = whiteShape;
+		if (hasPlace (pos) && isLegal(pos, color, _blackShape, _whiteShape)) {
+			makeMove (pos, color, blackShape, whiteShape);
+			afterMove (pos, color);
 			return true;
 		}
 		else return false;
 	}
 
-	void makeMove (C2DPoint pos, Constants.StoneColor color)
+	void makeMove (C2DPoint pos, Constants.StoneColor color, List<C2DPolyArc> blackShape, List<C2DPolyArc> whiteShape)
 	{
-		forbiddenShapes.Add (new C2DCircle (pos, Constants.stoneSize));
-
-		if (color == Constants.StoneColor.Black) {
-			toPlay = Constants.StoneColor.White;
-		} else {
-			toPlay = Constants.StoneColor.Black;
-		}
+		//TODO:implement
 	}
 
-	bool isLegal(C2DPoint pos, Constants.StoneColor color) {
-		//TODO: implement
-		return true;
+ /*
+	void makeMove (C2DPoint pos, Constants.StoneColor color, List<C2DPolyArc> blackShape, List<C2DPolyArc> whiteShape)
+	{
+		C2DPolyArc stoneShape = new C2DPolyArc(new C2DCircle (pos, 1));
+
+		List<C2DPolyArc> ownShape;
+		if (color == Constants.StoneColor.Black) {
+			ownShape = blackShape;
+		} else {
+			ownShape = whiteShape;
+		}
+
+		List<C2DPolyArc> shapesToMerge = new List<C2DPolyArc> ();
+		foreach (C2DPolyArc poly in ownShape) {
+			if (poly.Overlaps(stoneShape)) {
+				shapesToMerge.Add(poly);
+			}
+		}
+
+		merge(stoneShape, shapesToMerge, ownShape);
+
+	}
+
+	void merge(C2DPolyArc stoneShape, List<C2DPolyArc> shapesToMerge, List<C2DPolyArc> ownShape) {
+
+		Debug.Log (shapesToMerge [0].GetArea ());
+
+		C2DPolyArc stone = stoneShape;
+
+		foreach (C2DPolyArc shape in shapesToMerge) {
+			shapesToMerge.Remove(shape);
+		}
+
+		foreach (C2DPolyArc shape in shapesToMerge) {
+			shape.GetUnion(stone, shapesToMerge, new CGrid());
+		}
+
+		Debug.Log (shapesToMerge [0].GetArea ());
+	}
+*/
+	void afterMove (C2DPoint pos, Constants.StoneColor color)
+	{
+		forbiddenShapes.Add (new C2DCircle (pos, Constants.stoneSize));
+		if (color == Constants.StoneColor.Black) {
+			toPlay = Constants.StoneColor.White;
+		}
+		else {
+			toPlay = Constants.StoneColor.Black;
+		}
 	}
 
 	bool hasPlace(C2DPoint pos) {
@@ -56,4 +103,8 @@ public class LogicController : MonoBehaviour {
 		return true;
 	}
 
+	bool isLegal (C2DPoint pos, Constants.StoneColor color, List<C2DPolyArc> _blackShape, List<C2DPolyArc> _whiteShape)
+	{
+		return true;
+	}
 }

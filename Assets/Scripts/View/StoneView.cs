64c6115af0131398839e4e59c4fa52440f301c4c
projektorkraft <projektorkraft@gmail.com>
2014-05-24 15:34:51 +0200

    initial commit
    got board?

---
+++ Assets/Scripts/View/StoneView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneView : MonoBehaviour {
+
+	public Renderer backgroundRenderer;
+
+	// Use this for initialization
+	void Start () {
+		backgroundRenderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
